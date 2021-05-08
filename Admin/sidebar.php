14bf06f6276f00206b07bc7e3a402fd940843c11
miracziyaalev <miracziyaalev@gmail.com>
2021-05-08 15:26:49 +0300

    Update sidebar.php
    sidebar temizlendi. ikonlar eklendi

--- Admin/sidebar.php
+++ Admin/sidebar.php
@@ -25,17 +25,67 @@
                with font-awesome or any other icon font library -->
           <li class="nav-item">
             <a href="pages/widgets.html" class="nav-link">
-              <i class="nav-icon fas fa-th"></i>
+              <i class="fas fa-home"></i>
               <p>
                 Anasayfa
-                <span class="right badge badge-danger">New</span>
+                <span class="right badge badge-danger">Yeni</span>
+              </p>
+            </a>
+          </li>
+
+          <li class="nav-item">
+            <a href="pages/widgets.html" class="nav-link">
+              <i class="fas fa-bars"></i>
+              <p>
+                Kategoriler
+                <span class="right badge badge-danger">Yeni</span>
+              </p>
+            </a>
+          </li>
+
+          <li class="nav-item">
+            <a href="pages/widgets.html" class="nav-link">
+              <i class="fas fa-address-card"></i>
+              <p>
+                Hakkımızda
+                <span class="right badge badge-danger">Yeni</span>
+              </p>
+            </a>
+          </li>
+
+          <li class="nav-item">
+            <a href="pages/widgets.html" class="nav-link">
+              <i class="fas fa-images"></i>
+              <p>
+                Slider
+                <span class="right badge badge-danger">Yeni</span>
+              </p>
+            </a>
+          </li>
+
+          <li class="nav-item">
+            <a href="pages/widgets.html" class="nav-link">
+              <i class="fas fa-users"></i>
+              <p>
+                Üyeler
+                <span class="right badge badge-danger">Yeni</span>
+              </p>
+            </a>
+          </li>
+
+          <li class="nav-item">
+            <a href="pages/widgets.html" class="nav-link">
+              <i class="fas fa-comments"></i>
+              <p>
+                Yorumlar
+                <span class="right badge badge-danger">Yeni</span>
               </p>
             </a>
           </li>
 
           <li class="nav-item has-treeview menu-open">
             <a href="#" class="nav-link active">
-              <i class="nav-icon fas fa-tachometer-alt"></i>
+              <i class="fas fa-cog"></i>
               <p>
                 Ayarlar
                 <i class="right fas fa-angle-left"></i>
@@ -44,25 +94,25 @@
             <ul class="nav nav-treeview">
               <li class="nav-item">
                 <a href="./index.html" class="nav-link active">
-                  <i class="far fa-circle nav-icon"></i>
+                  <i class="fab fa-chrome"></i>
                   <p>Site Ayarları</p>
                 </a>
               </li>
               <li class="nav-item">
                 <a href="./index2.html" class="nav-link">
-                  <i class="far fa-circle nav-icon"></i>
+                  <i class="fas fa-id-card"></i>
                   <p>İletişim Ayarları</p>
                 </a>
               </li>
               <li class="nav-item">
                 <a href="./index3.html" class="nav-link">
-                  <i class="far fa-circle nav-icon"></i>
+                 <i class="fab fa-instagram"></i>
                   <p>Sosyal Medya Ayarları</p>
                 </a>
               </li>
               <li class="nav-item">
                 <a href="./index3.html" class="nav-link">
-                  <i class="far fa-circle nav-icon"></i>
+                  <i class="fas fa-users"></i>
                   <p>Kullanıcı Bilgileri</p>
                 </a>
               </li>
